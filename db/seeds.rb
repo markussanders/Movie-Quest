abb655867d631bd0c6ecb393a23426819db07cae
Derek Townsend <derekjavon2@gmail.com>
2019-05-28 16:26:45 -0500

    Condensed code in seeds a bit and cleared database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
 require 'rest-client'
 require 'JSON'
-# Movie.create(title: 'title', genre: 'genre', year: 'year', actors: 'actors', imdbRating: 2.4)
+
 movie_data = RestClient.get('http://www.omdbapi.com/?s=romantic&apikey=1d1382f6')
 parsed_movie_data = JSON.parse(movie_data)['Search']
 
-ids = parsed_movie_data.collect do |movie|
-  movie['imdbID']
-end
-
-ids.each do |id|
+parsed_movie_data.collect do |movie|
+  id = movie['imdbID']
   data = RestClient.get("http://www.omdbapi.com/?i=#{id}&apikey=1d1382f6")
-  parsed_data=JSON.parse(data)
-  rating = parsed_data["imdbRating"].to_f
-  Movie.create(title: parsed_data["Title"], genre: parsed_data["Genre"], year: parsed_data["Year"], actors: parsed_data["Actors"], imdbRating: rating)
+  parsed_data = JSON.parse(data)
+
+  title = parsed_data['Title']
+  genre = parsed_data['Genre']
+  year = parsed_data['Year']
+  actors = parsed_data['Actors']
+  imdbRating = parsed_data['imdbRating'].to_f
+
+  # Movie.create(title: title, genre: genre, year: year, actors: actors, imdbRating: imdbRating)
 end
