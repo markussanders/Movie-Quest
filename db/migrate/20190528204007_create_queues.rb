fa5f01b341a6a4bfd14c50c2ea8244f529e69ab7
John Linatoc <johnlinatoc1227@gmail.com>
2019-05-28 16:11:41 -0500

    created migrations for all three tables

---
+++ db/migrate/20190528204007_create_queues.rb
@@ -0,0 +1,8 @@
+class CreateQueues < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_queues do |t|
+      t.reference :movie
+      t.reference :user
+      t.timestamps
+  end
+end
