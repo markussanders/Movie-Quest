1fea79babbfb63441f96c89dbef7f742fa401cca
John Linatoc <johnlinatoc1227@gmail.com>
2019-05-29 10:52:34 -0500

    changed queue file name and added boilerplate methods for User

--- app/models/queue_selection.rb
+++ app/models/queue_selection.rb
@@ -1,4 +1,4 @@
-class QueueSelections < ActiveRecord::Base
+class QueueSelection < ActiveRecord::Base
     belongs_to :user
     belongs_to :movie
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ActiveRecord::Base
     has_many :queue_selections
-    has_many :movies, through: :queue_selection
+    has_many :movies, through: :queue_selections
+
+    def search_movie
+        
+    end
+
+    def get_movies
+        self.queue_selections
+    end
+
+    def add_queue_selection(queue_selection)
+
+    end
+
+    def remove_queue_selection(queue_selection)
+
+    end
+
 end
