f0b041995475da258d4d4e41702be948e9896aa0
John Linatoc <johnlinatoc1227@gmail.com>
2019-05-29 13:23:14 -0500

    Added search movie method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
     has_many :queue_selections
     has_many :movies, through: :queue_selections
 
-    def search_movie
-        
+    def search_movie(movie_input)
+        Movie.all.select do |movie|
+            movie[:title].downcase == movie_input.downcase
+        end
     end
 
     def get_movies
