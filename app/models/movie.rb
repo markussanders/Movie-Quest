d7fe7d021512112f53991bc756fc8c9d5eae2900
John Linatoc <johnlinatoc1227@gmail.com>
2019-05-29 16:08:27 -0500

    resolved merge conflicts from master merge and add user methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,6 @@
 class Movie < ActiveRecord::Base
     has_many :queue_selections
-<<<<<<< HEAD
     has_many :users, through: :queue_selection
-    
-    
-=======
     has_many :stars
     has_many :movie_genres
     has_many :users, through: :queue_selections
@@ -14,5 +10,5 @@ class Movie < ActiveRecord::Base
     def others_that_have_seen_movie
       self.users
     end
->>>>>>> 5609dcd6fd73835480d0881529670522e5c5c982
+
 end
