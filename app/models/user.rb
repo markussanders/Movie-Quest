b594af147e22ce9c153a61b395172dae43d6c18d
John Linatoc <johnlinatoc1227@gmail.com>
2019-05-29 15:42:36 -0500

    commented out view_queue method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,25 @@ class User < ActiveRecord::Base
     def search_movie(movie_input)
         Movie.all.select do |movie|
             movie[:title].downcase == movie_input.downcase
-        end
+        end[0]
     end
 
-    def get_movies
-        self.queue_selections
+    # def view_queue => amy.queue_selections
+    #     QueueSelection.all.select do |queue_selection|
+    #         queue_selection.all.user_id == self.user_id
+    #     end
+    # end
+
+    def add_queue_selection(movie)
+        new_movie_id = search_movie(movie).id
+        # binding.pry
+        QueueSelection.new(user_id: self.id, movie_id: new_movie_id).save
     end
 
-    def add_queue_selection(queue_selection)
-
-    end
-
-    def remove_queue_selection(queue_selection)
+    def remove_queue_selection(movie)
+        pending_deletion_movie_id = search_movie(movie).id
+        
+        self.queue_selections.find_by(movie_id: pending_deletion_movie_id).destroy
 
     end
 
