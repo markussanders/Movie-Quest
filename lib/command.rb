75d06f607ee8041f166445a8086d62e5880d9789
xmksz3 <markus.sanderst@gmail.com>
2019-05-29 15:25:24 -0500

    not working, mid refactor

--- lib/command.rb
+++ lib/command.rb
@@ -1,11 +1,21 @@
+require 'pry'
 class CommandLineInterface
 
   def greeting()
     puts "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
     puts "Hello! Please enter your first and last name."
     puts "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
-    full_name = gets.chomp
-    full_name
+  end
+  def full_name
+    gets.chomp.downcase
+  end
+
+  def existing_user?(full_name)
+    User.all.find_by(name: full_name) ? true : false
+  end
+
+  def current_user
+    User.all.find_or_create_by(name: get_name())
   end
 
   def validate_name(full_name)
@@ -15,20 +25,26 @@ class CommandLineInterface
       last_name = gets.chomp
       split_name << last_name
     end
-    puts "Thanks, #{split_name[0].downcase.capitalize}!"
-    # validated_name = split.name.join(' ')
-    # validated_name
+    first = split_name[0].downcase.capitalize
+    if existing_user?(split_name.join(' '))
+      puts "Welcome back, #{first}!"
+    else
+      puts "Welcome to BLANK, #{first}!"
+    end
+  end
+################################################################################
+  def search_for_movies()
+    user_input = gets.chomp
+    current_user.search_movie(user_input)
   end
 
-  def existing_user?(full_name)
-    User.all.find {|user| user.name == full_name}
-  end
 
+##############################prompts###########################################
   def help()
     puts "-------------------------------------"
-    puts "- help : displays this help message."
-    puts "- exit : exits this program."
-    puts "- menu : returns to main menu."
+    puts "- help : displays this help message.-"
+    puts "- exit : exits this program.        -"
+    puts "- menu : returns to main menu.      -"
     puts "-------------------------------------"
     choice = gets.chomp.downcase
   end
@@ -46,19 +62,20 @@ class CommandLineInterface
   end
 
   def exit()
-    puts "Goodbye (▰˘︹˘▰)"
+    return "Goodbye (▰˘︹˘▰)"
   end
-
+#################################runner#########################################
   def run()
-
     name = validate_name(greeting())
     name
     ########menu################
     menu_choice = menu()
     if menu_choice == '1' #search for movies
-
+      puts "Please enter the title of the movie you would like to search."
+      search_for_movies()
+      puts "Search Results:"
     elsif menu_choice == '2' #view current queue
-
+      # puts User.get_movies
     elsif menu_choice == '3' #modify queue
 
     elsif menu_choice == '4' #help
@@ -71,7 +88,7 @@ class CommandLineInterface
     end
 
     ##########help##############
-    help_choice = help()
+    help_choice = self.help()
     if help_choice == 'help'
       self.help()
     elsif help_choice == 'exit'
@@ -82,6 +99,5 @@ class CommandLineInterface
       puts "Invalid input. Please select an option from below:"
       self.help()
     end
-    return exit()
   end
 end
