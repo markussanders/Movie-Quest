def0403b6e59051a834a6e9dfca8e592c031acc1
Derek Townsend <derekjavon2@gmail.com>
2019-05-30 11:37:49 -0500

    All my changes and inclusions

--- lib/command.rb
+++ lib/command.rb
@@ -108,15 +108,15 @@ class CommandLineInterface
         add_movie_to_queue?(found_movie)
         self.run
       elsif menu_choice == '2' #search for movies by actor
-        found_movies = self.search_for_movies_by_actor
-        add_multiple_movies_to_queue?(found_movies)
+        self.search_for_movies_by_actor
+        add_multiple_movies_to_queue?
         self.run
       elsif menu_choice == '3' #search for movies by genre
-        found_movies = self.search_for_movies_by_genre
-        add_multiple_movies_to_queue?(found_movies)
+        self.search_for_movies_by_genre
+        add_multiple_movies_to_queue?
         self.run
       elsif menu_choice == '4' #view current queue
-        puts "#{self.get_current_queue}"
+        puts "#{self.other_options}"
         self.run
       elsif menu_choice == '5' #add to queue
         puts "Please enter the movie title you would like to add."
@@ -137,25 +137,34 @@ class CommandLineInterface
       end
   end
 
-  def add_multiple_movies_to_queue?(found_movies)
+  def add_multiple_movies_to_queue?
+    puts "-------------------------------------------------------------"
     puts "Would you like to one of these movie to your queue? (Y/N)"
+    puts "-------------------------------------------------------------"
     user_input = gets.chomp
     if user_input.downcase == 'y'
-      puts "Please enter the title of the movie you would like to search"
-      movie = gets.chomp
-      if current_user.search_movie(movie).nil?
+      puts "-------------------------------------------------------------"
+      puts "Please enter the title of the movie you would like to search."
+      puts "-------------------------------------------------------------"
+      new_input = gets.chomp
+      movie = current_user.search_movie(new_input)
+      if current_user.search_movie(new_input).nil?
         puts "Sorry, that title cant be found."
         # self.search_for_movies
         self.run
       end
       current_user.add_queue_selection(movie)
       puts "'#{movie.title}' has been added to your queue."
+    else
+      puts "Invalid input"
     end
-    puts self.menu
+    # puts self.menu
   end
 
   def search_for_movies_by_genre
+    puts "-------------------------------------------------------------"
     puts "Please enter the genre of the movie you would like to search."
+    puts "-------------------------------------------------------------"
     user_input = gets.chomp
     if Movie.all_by_genre(user_input)==[]
       puts "Sorry, that genre cant be found."
@@ -163,52 +172,36 @@ class CommandLineInterface
       self.run
     end
     found_movies = Movie.all_by_genre(user_input)
+    puts "-------------------------------------------------------------"
     puts "Search Results:"
+    puts "-------------------------------------------------------------"
     found_movies.each do |movie|
       puts movie.title
     end
     found_movies
   end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def other_options
-  puts "Would you like to do something else? (Y/N)"
-  user_input = gets.chomp.downcase
-  user_input == 'y' ? self.run : self.exit
+  def search_for_movies_by_actor
+    puts "-------------------------------------------------------------"
+    puts "Please enter the actor of the movie you would like to search."
+    puts "-------------------------------------------------------------"
+    user_input = gets.chomp
+    if Movie.all_by_actor(user_input)==[]
+      puts "Sorry, that actor cant be found."
+      # self.search_for_movies
+      self.run
+    end
+    found_movies = Movie.all_by_actor(user_input)
+    puts "-------------------------------------------------------------"
+    puts "Search Results:"
+    puts "-------------------------------------------------------------"
+    found_movies.each do |movie|
+      puts movie.title
+    end
+    found_movies
+  end
+  def other_options
+    puts "Would you like to do something else? (Y/N)"
+    user_input = gets.chomp.downcase
+    user_input == 'y' ? self.run : self.exit
+  end
 end
