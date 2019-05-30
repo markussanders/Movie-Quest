f0fc76f60b01d3f2d289441d7dccd3949c7846b7
xmksz3 <markus.sanderst@gmail.com>
2019-05-30 11:32:28 -0500

    CRUD

--- lib/command.rb
+++ lib/command.rb
@@ -47,34 +47,53 @@ class CommandLineInterface
     user_input = gets.chomp
     if current_user.search_movie(user_input).nil?
       puts "Sorry, that title cant be found."
-      # self.search_for_movies
-      self.run
     end
     found_movie = current_user.search_movie(user_input)
     puts "Search Results:"
     puts found_movie
+    found_movie
+  end
+
+  def list_movie_titles
+    puts "Your current queue includes:"
+    movies = current_user.queue_selections.map { |q|
+      Movie.where("id = ?", q.movie_id)
+    }.flatten.uniq
+    movies.each {|m| puts "--#{m.title}"}
   end
 
   def get_current_queue
-    puts "Your current queue includes:"
-    puts current_user.queue_selections
+    if current_user.queue_selections.length == 0
+      puts "There are no movies in your queue."
+      self.other_options
+    end
+    self.list_movie_titles
+    self.other_options
+  end
+
+  def select_movie_to_add
+    puts "Please enter the movie title you would like to add."
+    movie_to_add = gets.chomp
+    current_user.search_movie(movie_to_add)
   end
 
   def add_movie_to_queue?(movie)
+    # movie = self.movie_to_add
     puts "Would you like to add this movie to your queue? (Y/N)"
     user_input = gets.chomp
     if user_input.downcase == 'y'
       puts "'#{movie.title}' has been added to your queue."
       current_user.add_queue_selection(movie)
     end
-    self.run
+    self.other_options
   end
 
   def remove_movie_from_queue
-    self.get_current_queue
-    puts "Type the title of the movie you would like to remove."
+    puts "Type the title of the movie you would like to remove:"
+    self.list_movie_titles
     movie_title = gets.chomp
-    puts current_user.remove_queue_selections(movie_title)
+    puts current_user.remove_queue_selection(movie_title)
+    self.other_options
   end
 
   def help
@@ -101,74 +120,32 @@ class CommandLineInterface
     return
   end
 
+  def other_options
+    puts "Would you like to do something else? (Y/N)"
+    user_input = gets.chomp.downcase
+    user_input == 'y' ? self.run : (return self.exit)
+  end
+
   def run
     menu_choice = self.menu
       if menu_choice == '1' #search for movies
         found_movie = self.search_for_movies
         add_movie_to_queue?(found_movie)
-        self.run
+        self.other_options
       elsif menu_choice == '4' #view current queue
-        puts "#{self.get_current_queue}"
-        self.run
+        self.get_current_queue
       elsif menu_choice == '5' #add to queue
-        puts "Please enter the movie title you would like to add."
-        movie_to_add = gets.chomp
-        found_movie = current_user.search_movie(movie_to_add)
-        self.add_movie_to_queue?(found_movie)
-        puts "Added #{movie_to_add.title} to your queue."
-        self.run
+        self.add_movie_to_queue?(select_movie_to_add)
       elsif menu_choice == '6' #modify queue
-        puts self.remove_movie_from_queue
+        self.remove_movie_from_queue
       elsif menu_choice == '7' #help
         self.help
       elsif menu_choice == '8' #exit
         self.exit
       else
         puts "Invalid input. Please select an option from below:"
-        self.menu
+        self.run
       end
   end
 
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def other_options
-  puts "Would you like to do something else? (Y/N)"
-  user_input = gets.chomp.downcase
-  user_input == 'y' ? self.run : self.exit
 end
