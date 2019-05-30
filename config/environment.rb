947a848442bd2dd550ee1017b292b4936b0a3f96
xmksz3 <markus.sanderst@gmail.com>
2019-05-30 12:55:39 -0500

    Adding sql ignore

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-ActiveRecord::Base.logger = nil
 require 'bundler'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.logger = nil
 require_all 'lib'
 require_all 'app'
 
